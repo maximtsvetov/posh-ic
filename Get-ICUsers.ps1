7a6df149472e1b70640dc5f80cbe80870c8f4c6e
plmcgrn <paul.mcgurn@gmail.com>
2020-03-20 00:01:36 -0400

    PSCore compat

--- Get-ICUsers.ps1
+++ Get-ICUsers.ps1
@@ -1,11 +1,9 @@
 <#
-# AUTHOR : Pierrick Lozach
+# AUTHOR : Pierrick Lozach, extended by Paul McGurn
 #>
 
-function Get-ICUsers() # {{{2
-{
-# Documentation {{{3
-<#
+function Get-ICUsers() {
+  <#
 .SYNOPSIS
   Gets a list of all users
 .DESCRIPTION
@@ -15,15 +13,15 @@ function Get-ICUsers() # {{{2
 #> # }}}3
   [CmdletBinding()]
   Param(
-    [Parameter(Mandatory=$true)]  [Alias("Session", "Id")] [ININ.ICSession] $ICSession
+    [Parameter(Mandatory = $true)]  [Alias("Session", "Id")] $ICSession
   )
 
   $headers = @{
     "Accept-Language"      = $ICSession.language;
     "ININ-ICWS-CSRF-Token" = $ICSession.token;
   }
-  $response = Invoke-RestMethod -Uri "$($ICsession.baseURL)/$($ICSession.id)/configuration/users" -Method Get -Headers $headers -WebSession $ICSession.webSession -ErrorAction Stop
-  Write-Output $response | Format-Table
-  [PSCustomObject] $response
-} # }}}2
+  $response = Invoke-RestMethod -Uri "$($ICsession.baseURL)/$($ICSession.id)/configuration/users?select=*" -Method Get -Headers $headers -WebSession $ICSession.webSession -ErrorAction Stop
+  
+  return $response
+} 
 
