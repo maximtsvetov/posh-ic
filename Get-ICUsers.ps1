a94376dfd32ce31ac386d66c8da854050f4ac110
plmcgrn <paul.mcgurn@gmail.com>
2020-06-01 18:23:25 -0400

    Reworked for handling large user lists * Added support to query specific Properties/Fields * Added support for Content-Range to get large user lists (>200 users) * Code could use some simplification, tested functional against 2019R1

--- Get-ICUsers.ps1
+++ Get-ICUsers.ps1
@@ -27,6 +27,7 @@ function Get-ICUsers() {
 
   #we'll use the supplied properties (can be "*") to get detailed results, if the param was supplied
   if (![String]::IsNullOrEmpty($properties)) {
+    Write-Verbose "Called with specific properties, will use 'select' querystring"
     $uri = "$($ICsession.baseURL)/$($ICSession.id)/configuration/users?select=${properties}"
   }
 
@@ -36,12 +37,78 @@ function Get-ICUsers() {
     -WebSession $ICSession.webSession `
     -ResponseHeadersVariable responseheaders `
     -ErrorAction Stop  
+  Write-Verbose "First API call result size: ${response.items.count}"
 
-  #TODO: Add Content-Range handling for result sets > 200 items
-  if (!([String]::IsNullOrEmpty($responseheaders.'Content-Range'))) {
-    Write-Verbose "NOTE: API indicated result set is largger than a single API call, results may be truncated"
+  $contentrange = $responseheaders.'Content-Range'
+
+  $results = [System.Collections.ArrayList]@()
+
+  foreach ($i in $response.items) {
+    $results.add($i)>$null
   }
+  $pcr = parseContentRange($contentrange)
 
-  return $response.items
+  if ($pcr.total -gt 200) {
+    Write-Verbose "Submitting API calls for additional users"
+    #we have more API calls to submit
+    foreach ($r in $pcr.AdditionalRanges) {
+      Write-Verbose "Getting Users in range: ${r}"            
+
+      $additionalrequestheaders = @{
+        "Accept-Language"      = $ICSession.language;
+        "ININ-ICWS-CSRF-Token" = $ICSession.token;
+        "Range"                = "items=" + $r;
+      }
+      $headerstring = $additionalrequestheaders | ConvertTo-Json -Depth 4
+      Write-Verbose "Request Headers: ${headerstring}"
+
+      $additionalresponse = Invoke-RestMethod -Uri $uri `
+        -Method Get `
+        -Headers $additionalrequestheaders `
+        -WebSession $ICSession.webSession `
+        -ResponseHeadersVariable responseheaders `
+        -ErrorAction Stop  
+      $size = $additionalresponse.items.count
+      Write-Verbose "Size for ${r} is ${size}"
+      $responseheaders = $responseheaders | ConvertTo-Json -Depth 8
+      Write-Verbose "Response headers: ${responseheaders}"
+
+      foreach ($i in $additionalresponse.items) {
+        $results.add($i)>$null
+      }
+    }
+  }
+  return $results
+
+
 } 
+function parseContentRange($contentrange) {
+  #ex. items 1-199/1050
+  $range = $contentrange.split(" ")[1]
+  $total = [Int32]$range.split("/")[1]
+  Write-Verbose "Total count of users: ${total}"
+  $wholesets = [Math]::Floor($total / 200)
+  $lastrangestart = $wholesets * 200
+  $result = [PSCustomObject]@{        
+    Total = $total
+  }
+  if ($total -gt 200) {
+    Write-Verbose "Ranges are greater than 200"
+    $ranges = [System.Collections.ArrayList]@()
+    
+    #start with second range, as the initial API call will get the first
+    for ($i = 1; $i -lt $wholesets; $i++) {
+      $rangestart = $i * 200
+      $rangeend = $rangestart + 199
+
+      $rangestring = "${rangestart}-${rangeend}"
+      $ranges.add($rangestring)>$null        
+    }
+    #add the last partial range
+    $lastrange = "${lastrangestart}-${total}"
+    $ranges.add($lastrange)>$null
+    $result | Add-Member -NotePropertyName AdditionalRanges -NotePropertyValue $ranges
+  }    
+  return $result
+}
 
